1d2de9677b98306aae3b64c09b9d034c11c91f2a
KingMaster86 <kingmastermassage2@gmail.com>
2025-07-03 14:26:55 +0700

    Update admin-panel.php

--- admin-panel/admin-panel.php
+++ admin-panel/admin-panel.php
@@ -1,161 +1,73 @@
-<?php
-include('../includes/connect.php');
-session_start();
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dashboard | TAXI PI</title>
+  <link rel="shortcut icon" href="../assets/img/taxi-img.png" type="image/x-icon" />
+  
+  <!-- PWA Requirements -->
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="theme-color" content="#5E2B97" />
+  <script>
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js');
+    }
+  </script>
 
-    <!-- Fav Icon -->
-    <link rel="shortcut icon" href="../assets/img/taxi-img.png" type="image/x-icon" class="object-fit-cover" />
-    <title>Dashboard | CityTaxi</title>
-
-    <!-- Google Font (Sen) -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;500;600;700&display=swap" rel="stylesheet" />
-
-    <!-- Just Validate Dev CDN -->
-    <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js"></script>
-
-    <!-- Font Awesome CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
-
-    <!-- Bootsrap CSS -->
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
-
-    <!-- External CSS -->
-    <link rel="stylesheet" href="../assets/css/style.css" />
-    <link rel="stylesheet" href="../assets/css/style2.css">
-    <link rel="stylesheet" href="../assets/css/style3.css">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;500;600;700&display=swap" rel="stylesheet" />
+  
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+  <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../assets/css/style.css" />
+  <link rel="stylesheet" href="../assets/css/style2.css">
+  <link rel="stylesheet" href="../assets/css/style3.css">
 </head>
-
 <body class="overflow-x-hidden container-fluid bg-warning">
-    <div>
-        <div class="row">
-            <!-- Side Bar -->
-            <div class="col-md-12 container background-black-color px-3 py-4">
-                <!-- Admin Profile Pic -->
-                <div class="d-md-flex justify-content-center align-items-center gap-2">
-                    <div>
-                        <img src="../assets/img/406880331_369106048863056_7316214919984039805_n.jpg" class="img-fluid width-of-admin-prof" alt="Admin Profile Picture" />
-                    </div>
-                    <div>
-                        <h3 class="text-warning fw-semibold">Admin</h3>
-                    </div>
-                </div>
+  <div class="row">
+    <!-- Sidebar -->
+    <div class="col-md-12 container background-black-color px-3 py-4">
+      <!-- Admin Profile -->
+      <div class="d-md-flex justify-content-center align-items-center gap-2">
+        <img src="../assets/img/406880331_369106048863056_7316214919984039805_n.jpg" class="img-fluid width-of-admin-prof" alt="Admin Profile" />
+        <h3 class="text-warning fw-semibold">Admin</h3>
+      </div>
 
-                <!-- Admin Dashboard Menu -->
-                <div class="d-flex justify-content-center align-items-center gap-5 ">
-                    <!-- Overview -->
-                    <a href="admin-panel.php?dashboard" class="d-md-flex align-items-center hover-effect text-light  gap-3 btn mt-4">
-                        <!-- Icon -->
-                        <div>
-                            <i class="fa-solid fa-gauge fs-5"></i>
-                        </div>
-
-                        <!-- Menu Name -->
-                        <p class="fs-5 mb-1">Overview</p>
-                    </a>
-
-                    <!-- Passengers -->
-                    <a href="admin-panel.php?passengers" class="d-md-flex align-items-center hover-effect text-light gap-3 btn mt-4">
-                        <!-- Icon -->
-                        <div>
-                            <i class="fa-solid fa-users"></i>
-                        </div>
-
-                        <!-- Menu Name -->
-                        <p class="fs-5 mb-1">Passengers</p>
-                    </a>
-
-                    <!-- Drivers -->
-                    <a href="admin-panel.php?drivers" class="d-md-flex align-items-center hover-effect text-light gap-3 btn mt-4">
-                        <!-- Icon -->
-                        <div>
-                            <i class="fa-solid fa-id-card"></i>
-                        </div>
-
-                        <!-- Menu Name -->
-                        <p class="fs-5 mb-1">Drivers</p>
-                    </a>
-
-                    <!-- Completed Reservations -->
-                    <a href="admin-panel.php?completed_reservations" class="d-md-flex align-items-center hover-effect text-light gap-3 btn mt-4">
-                        <!-- Icon -->
-                        <div>
-                            <i class="fa-solid fa-check"></i>
-                        </div>
-
-                        <!-- Menu Name -->
-                        <p class="fs-5 mb-1">Completed Reservations</p>
-                    </a>
-
-                    <!-- Logout -->
-                    <a href="../index.php" class="d-md-flex align-items-center hover-effect text-light gap-3 btn mt-4">
-                        <!-- Icon -->
-                        <div>
-                            <i class="fa-solid fa-home"></i>
-                        </div>
-
-                        <!-- Menu Name -->
-                        <p class="fs-5 mb-1">Home</p>
-                    </a>
-                    <!-- Home -->
-                    <a href="logout.php" class="d-md-flex align-items-center hover-effect text-light gap-3 btn mt-4">
-                        <!-- Icon -->
-                        <div>
-                            <i class="fa-solid fa-right-from-bracket"></i>
-                        </div>
-
-                        <!-- Menu Name -->
-                        <p class="fs-5 mb-1">Logout</p>
-                    </a>
-                </div>
-            </div>
-
-
-        </div>
-
-        <!-- Dashboard Contents -->
-        <div class="col-md-12 bg-warning ">
-            <!-- PHP Code Dynamcially Changing Layouts -->
-            <?php
-            if (isset($_GET['dashboard'])) {
-                include('dashboard.php');
-            }
-            if (isset($_GET['passengers'])) {
-                include('total-passenger.php');
-            }
-
-            if (isset($_GET['drivers'])) {
-                include('drivers.php');
-            }
-
-            if (isset($_GET['edit_passenger'])) {
-                include('edit-passenger.php');
-            }
-
-            if (isset($_GET['edit_driver'])) {
-                include('edit-driver.php');
-            }
-
-            if (isset($_GET['completed_reservations'])) {
-                include('show-completed-reservations.php');
-            }
-            ?>
-        </div>
+      <!-- Menu -->
+      <div class="d-flex justify-content-center align-items-center gap-5">
+        <a href="dashboard.html" class="btn mt-4 text-light d-md-flex align-items-center gap-3 hover-effect">
+          <i class="fa-solid fa-gauge fs-5"></i>
+          <p class="fs-5 mb-1">Overview</p>
+        </a>
+        <a href="total-passenger.html" class="btn mt-4 text-light d-md-flex align-items-center gap-3 hover-effect">
+          <i class="fa-solid fa-users"></i>
+          <p class="fs-5 mb-1">Passengers</p>
+        </a>
+        <a href="drivers.html" class="btn mt-4 text-light d-md-flex align-items-center gap-3 hover-effect">
+          <i class="fa-solid fa-id-card"></i>
+          <p class="fs-5 mb-1">Drivers</p>
+        </a>
+        <a href="show-completed-reservations.html" class="btn mt-4 text-light d-md-flex align-items-center gap-3 hover-effect">
+          <i class="fa-solid fa-check"></i>
+          <p class="fs-5 mb-1">Completed</p>
+        </a>
+        <a href="../index.html" class="btn mt-4 text-light d-md-flex align-items-center gap-3 hover-effect">
+          <i class="fa-solid fa-home"></i>
+          <p class="fs-5 mb-1">Home</p>
+        </a>
+        <a href="logout.html" class="btn mt-4 text-light d-md-flex align-items-center gap-3 hover-effect">
+          <i class="fa-solid fa-right-from-bracket"></i>
+          <p class="fs-5 mb-1">Logout</p>
+        </a>
+      </div>
     </div>
+  </div>
 
-    <!-- Boostrap JS Files -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    <!-- End -->
+  <!-- Konten dashboard dinamis dipindahkan ke file HTML terpisah -->
+
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"></script>
 </body>
-
 </html>
